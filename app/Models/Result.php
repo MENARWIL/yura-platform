5d7c90d4246f2461a3533562a5d3d84948bb3faa
MENARWIL <mendietaarnezwilson@gmail.com>
2026-09-04 00:02:47 -0400

    modelo para guardar resultados de las practicas

---
+++ app/Models/Result.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Result extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'session_id',
+        'student_id',
+        'score',
+        'feedback',
+    ];
+
+    public function session(): BelongsTo
+    {
+        return $this->belongsTo(ActivitySession::class, 'session_id');
+    }
+
+    public function student(): BelongsTo
+    {
+        return $this->belongsTo(Student::class, 'student_id');
+    }
+}
