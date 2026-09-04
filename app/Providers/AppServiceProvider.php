059d07c6a1c952aa02baaf57c3f33c20807c06c8
MENARWIL <mendietaarnezwilson@gmail.com>
2026-09-04 00:05:44 -0400

    subiendo los service providers de la aplicacion

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\ServiceProvider;
+use App\Models\Grade;
+use App\Observers\GradeObserver;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Grade::observe(GradeObserver::class);
+
+        RateLimiter::for('login', function (Request $request) {
+            return Limit::perMinute(5)->by($request->ip());
+        });
+
+        RateLimiter::for('global', function (Request $request) {
+            return Limit::perMinute(60)->by($request->ip());
+        });
+    }
+}
